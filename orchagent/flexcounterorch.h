a8a28a84c21f3351d5c0b65c00fe74fb4ca81981
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2025-02-14 23:05:12 +0200

    [FC] process FC after apply view (#3326)
    * [FC] process FC after apply view
    
    What I did
    Simplify approach to delaying counters on warm boot and fast boot. Removed FLEX_COUNTER_DELAY_STATUS_FIELD and instead postpone all FC processing to happen after apply view to not delay data plane configuration.
    
    The CONFIG_DB should not be updated in runtime anymore for counters to be delayed.
    
    Why I did it
    To address sonic-net/sonic-buildimage#20302.
    
    How I verified it
    Run warm-boot - make sure FC orch runs only after APPLY_VIEW.

--- orchagent/flexcounterorch.h
+++ orchagent/flexcounterorch.h
@@ -4,6 +4,7 @@
 #include "orch.h"
 #include "port.h"
 #include "producertable.h"
+#include "selectabletimer.h"
 #include "table.h"
 
 extern "C" {
@@ -40,6 +41,7 @@ class FlexCounterOrch: public Orch
 {
 public:
     void doTask(Consumer &consumer);
+    void doTask(SelectableTimer &timer);
     FlexCounterOrch(swss::DBConnector *db, std::vector<std::string> &tableNames);
     virtual ~FlexCounterOrch(void);
     bool getPortCountersState() const;
@@ -63,10 +65,12 @@ private:
     bool m_pg_watermark_enabled = false;
     bool m_hostif_trap_counter_enabled = false;
     bool m_route_flow_counter_enabled = false;
-    Table m_flexCounterConfigTable;
+    bool m_delayTimerExpired = false;
     Table m_bufferQueueConfigTable;
     Table m_bufferPgConfigTable;
     Table m_deviceMetadataConfigTable;
+    std::unique_ptr<SelectableTimer> m_delayTimer;
+    std::unique_ptr<Executor> m_delayExecutor;
     std::unordered_set<std::string> m_groupsWithBulkChunkSize;
 };
 
